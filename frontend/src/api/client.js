11f174fbb67d4c85d293147d24afe6760a2e07fc
i-ayushm <ayush.mishra@prayasfinance.com>
2026-03-03 13:21:51 +0530

    update api_call

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://hrms-lite-lizn.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
